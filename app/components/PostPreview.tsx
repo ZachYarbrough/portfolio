d1c618a47c75a5cb0391bbf2c9f1cac9783b365a
ZachYarbrough <zachyarbro@gmail.com>
2025-01-28 19:47:16 -0600

    Made tags more responsive in postPreview component

--- app/components/PostPreview.tsx
+++ app/components/PostPreview.tsx
@@ -1,16 +1,35 @@
+'use client'
+
+import { useState, useEffect } from 'react'
 import { PostMetadata } from '../types/posts'
 import InternalLink from './InternalLInk'
 
 const PostPreview = ({ slug, title, date, tags }: PostMetadata) => {
+        const [width, setWidth] = useState(1501)
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth)
+        }
+
+        window.addEventListener("resize", handleResize)
+
+        handleResize()
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [setWidth])
+
     return (
-        <div key={slug} style={{ display: 'grid', gridTemplateColumns: '6rem 3.5fr 4fr', width: '100%', margin: '1rem 0' }}>
+        <div key={slug} style={{ display: 'grid', gridTemplateColumns: width > 600 ? '6rem 3.5fr 4fr' : '6rem 8fr', width: '100%', margin: '1rem 0' }}>
             <p className='text-secondary'>{date}</p>
             <div>
                 <InternalLink href={`/posts/${slug}`}>
                     <h2 className='text-highlight font-bold' style={{ paddingLeft: '1rem' }}>{title}</h2>
                 </InternalLink> 
             </div>
-            <div className='flex' style={{ flexWrap: 'wrap', gap: '0.5rem', justifyContent: 'flex-end' }}>
+	    {width > 600 && <div className='flex' style={{ whiteSpace: 'nowrap', gap: '0.5rem', justifyContent: 'flex-end' }}>
                 {tags.map((tag) => (
                     <div key={tag}>
                         <InternalLink key={tag} useBubbleStyle={true} href={`/tags/${tag}`}
@@ -19,7 +38,7 @@ const PostPreview = ({ slug, title, date, tags }: PostMetadata) => {
                         </InternalLink>
                     </div>
                 ))}
-            </div>
+            </div>}
         </div>
     )
 }
