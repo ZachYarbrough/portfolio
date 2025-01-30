da5778f2b0a376c9af1aa90c1b8bedee43d651ed
ZachYarbrough <zachyarbro@gmail.com>
2025-01-29 20:59:53 -0600

    Added scroll when copying a post header

--- app/components/PostHeader.tsx
+++ app/components/PostHeader.tsx
@@ -1,25 +1,41 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { LinkIcon } from './assets/icons'
 import { copyToClipboard } from './general';
 import '@/app/globals.css'
 
 const PostHeader = ({ headerNumber, children }: { headerNumber: number, children: React.ReactNode }) => {
+    const ref = useRef(null)
+    const header = '#' + children?.toString().replace(/\s+/g, '%20') 
     const [showCopy, setShowCopy] = useState(false)
 
     const handleCopy = async () => {
-        await copyToClipboard(window.location.origin + window.location.pathname + '#' + children?.toString().replace(/\s+/g, '%20'))
+        await copyToClipboard(window.location.origin + window.location.pathname + header)
     }
 
+    useEffect(() => {
+	if (window.location.hash.includes(header)) {
+	    const element = document.getElementById(header)
+
+	    console.log(element)
+	    if (!element) return;
+
+	    const timeoutId = setTimeout(() => element.scrollIntoView({ behavior: 'smooth' }), 100); // Adjust the delay as needed
+
+	    // Cleanup the timeout if the component unmounts
+	    return () => clearTimeout(timeoutId);
+	    }
+    }, [])
+
     return (
-        <div onMouseEnter={() => setShowCopy(true)} onMouseLeave={() => setShowCopy(false)} className='flex items-center gap-2'>
+        <div ref={ref} id={header} onMouseEnter={() => setShowCopy(true)} onMouseLeave={() => setShowCopy(false)} className='flex items-center gap-2'>
             <h1 id={children?.toString()} className='text-primary' style={{
                 padding: '0.5rem 0',
                 fontSize: `${headerNumber}rem`,
                 fontWeight: '700',
             }}>{children}</h1>
-            {showCopy && <span onClick={handleCopy} className='cursor-pointer hover:text-highlight'><LinkIcon /></span>}
+            {showCopy && <a onClick={handleCopy} href={header} className='cursor-pointer hover:text-highlight'><LinkIcon /></a>}
         </div>
     )
 }
