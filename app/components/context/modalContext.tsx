2873741e5eb0edbc6da704c251f919fdc63a77d6
ZachYarbrough <zachyarbro@gmail.com>
2025-08-16 12:35:52 -0500

    Removed duplicate height from image modal

---
+++ app/components/context/modalContext.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { createContext, useState } from 'react';
+
+type Image = {
+    src: string,
+    alt: string
+}
+
+export const ModalContext = createContext({
+    imageObj: {
+	src: '',
+	alt: ''
+    },
+    toggleModal: (src?: string, alt?: string) => { }
+})
+
+export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
+    const [imageObj, setImageObj] = useState<Image>({
+	src: '',
+	alt: ''
+    })
+
+    const toggleModal = (src?: string, alt?: string) => {
+	setImageObj({
+	    src: src || '',
+	    alt: alt || ''
+	})
+	console.log(src)
+    }
+
+    return (
+        <ModalContext.Provider value={{
+            imageObj,
+            toggleModal
+        }}>
+            {children}
+        </ModalContext.Provider>
+    )
+}
---
+++ app/components/imageModal.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import React, { useCallback, useContext, useEffect, useState } from "react"
+import { ModalContext } from "./context/modalContext"
+
+const ImageModal = () => {    
+    const { imageObj, toggleModal } = useContext(ModalContext)
+    const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth)
+        }
+
+        window.addEventListener("resize", handleResize)
+
+        handleResize()
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [setWidth])
+
+
+    const escFunction = useCallback((event: KeyboardEvent) => {
+	if (event.key === "Escape") {
+	    toggleModal()
+	}
+    }, []);
+
+    useEffect(() => {
+	document.addEventListener("keydown", escFunction, false);
+
+	return () => {
+	    document.removeEventListener("keydown", escFunction, false);
+	};
+    }, [escFunction]);
+
+    useEffect(() => {
+	if (width <= 1500) toggleModal()
+    }, [width])
+
+    return (
+	<>
+	{imageObj.src != '' && <div style={{ zIndex: 1000, width: '100vw', height: '100vh', position: 'fixed', display: 'flex', justifyContent: 'center', alignItems: 'top', top: 0, left: 0 }}>
+	    <div style={{ position: 'absolute', width: '100vw', height: '100vh', backgroundColor: 'black', opacity: 0.3, cursor: 'pointer' }} onClick={() => toggleModal('')} />
+		<div style={{
+                    zIndex: 1001,
+                    width: '80%',
+		    maxHeight: '80%',
+                    margin: 'auto',
+		    display: 'flex'
+                }}>
+		    <div style={{ width: '100%', height: '100%', borderRadius: '0.5rem', border: '1px solid var(--background)', padding: '0.5rem 1rem', backgroundColor: 'var(--background)' }}>
+			<img src={imageObj.src} alt={imageObj.alt} style={{ width: '100%', height: '100%', borderRadius: '0.5rem', border: '1px solid var(--secondary-light)', margin: '0.5rem 0 0.5rem 0' }} loading="lazy" />
+		    </div>
+		</div>
+	    </div>
+	}
+	</>
+    )
+}
+
+export default ImageModal
