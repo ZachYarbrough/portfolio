ab15c87167f53a48356cb1f9d271bb36faf1c29e
ZachYarbrough <zachyarbro@gmail.com>
2025-08-18 10:14:07 -0500

    Fixed build errors

--- app/components/context/searchContext.tsx
+++ app/components/context/searchContext.tsx
@@ -7,8 +7,8 @@ type SearchContextType = {
   toggleSearch: (toggle?: boolean, index?: number) => void
   highlightedIndex: number,
   setHighlightedIndex: React.Dispatch<React.SetStateAction<number>>,
-  registerSearchRef: (inputRef: React.RefObject<HTMLInputElement>) => void,
-  searchRef: React.RefObject<HTMLInputElement> | null
+  registerSearchRef: (inputRef: React.RefObject<HTMLInputElement | null> | null) => void,
+  searchRef: React.RefObject<HTMLInputElement | null> | null
 
 }
 
@@ -17,21 +17,21 @@ export const SearchContext = createContext<SearchContextType>({
     toggleSearch: (toggle?: boolean, index?: number) => { },
     highlightedIndex: -1,
     setHighlightedIndex: () => {},
-    registerSearchRef: (inputRef: React.RefObject<HTMLInputElement>) => {},
+    registerSearchRef: (inputRef: React.RefObject<HTMLInputElement | null> | null) => { },
     searchRef: null 
 })
 
 export const SearchProvider = ({ children }: { children: React.ReactNode }) => {
     const [searchToggle, setSearchToggle] = useState<boolean>(false)
     const [highlightedIndex,setHighlightedIndex] = useState<number>(-1)
-    const [searchRef, setSearchRef] = useState<React.RefObject<HTMLInputElement> | null>(null)
+    const [searchRef, setSearchRef] = useState<React.RefObject<HTMLInputElement | null> | null>(null)
 
     const toggleSearch = (toggle: boolean = false, index: number = -1) => {
         setSearchToggle((prev) => typeof toggle === 'boolean' ? toggle : !prev)
 	setHighlightedIndex(index)
     }
 
-    const registerSearchRef = (inputRef: React.RefObject<HTMLInputElement>) => {
+    const registerSearchRef = (inputRef: React.RefObject<HTMLInputElement | null> | null) => {
 	  setSearchRef(inputRef)
     }
 
