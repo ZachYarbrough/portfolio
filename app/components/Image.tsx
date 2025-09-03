0abe4c96173b37d07fad9e9f6febc6d0145c9e1e
ZachYarbrough <zachyarbro@gmail.com>
2025-09-03 15:16:14 -0500

    Converted image component to use nextjs server side image

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -1,14 +1,17 @@
 'use client' 
 import React, { useContext } from 'react'
 import { ModalContext } from './context/modalContext'
+import NextImage from './ServerImage'
 
 const Image = (props: any) => {
     const { toggleModal } = useContext(ModalContext)
 
-    return (
-	<>
-	    <img src={props.src} alt={props.alt} title={props?.title || ''} className={props?.className || ''} style={{ ...props?.style, cursor: !props?.hideModal ? 'pointer' : 'default' }} loading="lazy" onClick={() => !props?.hideModal && toggleModal(props.src, props.alt) } />
-	</>
-    )
+  const handleClick = () => {
+    if (!props.hideModal) {
+      toggleModal(props.src, props.alt)
+    }
+  }
+
+    return <NextImage {...props} onClick={handleClick} />
 }
 export default Image
---
+++ app/components/ServerImage.tsx
@@ -0,0 +1,32 @@
+import NextImage from 'next/image'
+
+interface ImageProps {
+  src: string
+  alt: string
+  title?: string
+  className?: string
+  style?: React.CSSProperties
+  hideModal?: boolean
+  onClick?: () => void
+}
+
+const Image = (props: ImageProps) => {
+  const { src, alt, title, className, style, hideModal, onClick } = props
+
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      title={title || ''}
+      className={className}
+      style={{ ...style, cursor: !hideModal ? 'pointer' : 'default' }}
+      width={600}
+      height={400}
+      onClick={onClick}
+      loading="lazy"
+      priority={false}
+    />
+  )
+}
+
+export default Image
