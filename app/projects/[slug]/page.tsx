5d727f56d466cbf74f9d0a2694661a596f1e0425
ZachYarbrough <zachyarbro@gmail.com>
2025-09-27 15:49:52 -0500

    Converted not found from app to page router

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -33,24 +33,24 @@ export const generateStaticParams = async () => {
 }
 
 export async function generateMetadata({ params }: any) {
-  const post = getProjectContent(params.slug)
-  return {
-    title: post.title + ' | Zach Yarbrough',
-    description: post.description,
-  }
+    const post = getProjectContent(params.slug)
+    return {
+	title: post?.title + ' | Zach Yarbrough',
+	description: post?.description,
+    }
 }
 
 const getRelativePostContent = (slug: string): any => {
-  const folder = 'posts/'
-  const file = `${folder}${slug}.md`
-  const content = fs.readFileSync(file, 'utf8')
-  const matterResult = matter(content)
+    const folder = 'posts/'
+    const file = `${folder}${slug}.md`
+    const content = fs.readFileSync(file, 'utf8')
+    const matterResult = matter(content)
 
-  return {
-    title: matterResult.data.title,
-    slug: slug,
-    folder: 'posts'
-  }
+    return {
+	title: matterResult.data.title,
+	slug: slug,
+	folder: 'posts'
+    }
 } 
 
 /**
@@ -61,35 +61,35 @@ const getRelativePostContent = (slug: string): any => {
 */
 const getProjectContent = (slug: string): Project | null => {
     try {
-    const folder = 'projects/'
-    const file = `${folder}${slug}.md`
-    const content = fs.readFileSync(file, 'utf8')
-    const matterResult = matter(content)
+	const folder = 'projects/'
+	const file = `${folder}${slug}.md`
+	const content = fs.readFileSync(file, 'utf8')
+	const matterResult = matter(content)
 
-    // Get headers with regex that matches the format '# Header', '## Header', etc.
-    const headers = (matterResult.content + '\n').match(/(#+ .*\n)/g) || []
-    const tableOfContents = getTableOfContents(headers, true)
+	// Get headers with regex that matches the format '# Header', '## Header', etc.
+	const headers = (matterResult.content + '\n').match(/(#+ .*\n)/g) || []
+	const tableOfContents = getTableOfContents(headers, true)
 
-    const relatedPosts = matterResult.data.related ? matterResult.data.related.map((post: any) => getRelativePostContent(post)) : []
+	const relatedPosts = matterResult.data.related ? matterResult.data.related.map((post: any) => getRelativePostContent(post)) : []
 
-    return {
-	title: matterResult.data.title,
-	description: matterResult.data.description,
-	date: formatDate(matterResult.data.date),
-	tags: matterResult.data.tags,
-	headers: headers,
-	tableOfContents: tableOfContents,
-	technologyUsed: matterResult.data.technology_used || [],
-	preview: matterResult.data.preview,	
-	gallery_path: matterResult.data.gallery_path,
-	gallery: getGalleryImages(matterResult.data.gallery_path),
-	timeToRead: getTimeToRead(matterResult.content),
-	slug: slug,
-	related: relatedPosts,
-	content: matterResult.content,
-	source: matterResult.data.source,
-	live: matterResult.data.live
-    }
+	return {
+	    title: matterResult.data.title,
+	    description: matterResult.data.description,
+	    date: formatDate(matterResult.data.date),
+	    tags: matterResult.data.tags,
+	    headers: headers,
+	    tableOfContents: tableOfContents,
+	    technologyUsed: matterResult.data.technology_used || [],
+	    preview: matterResult.data.preview,	
+	    gallery_path: matterResult.data.gallery_path,
+	    gallery: getGalleryImages(matterResult.data.gallery_path),
+	    timeToRead: getTimeToRead(matterResult.content),
+	    slug: slug,
+	    related: relatedPosts,
+	    content: matterResult.content,
+	    source: matterResult.data.source,
+	    live: matterResult.data.live
+	}
     } catch (err) {
 	console.log(err)
 	return null	
@@ -106,48 +106,48 @@ const ProjectPage = async ({ params }: any) => {
     const { slug } = await params
     const project: Project | null = getProjectContent(slug)
 
-    if (!project) notFound()
+    if (!project) return { notFound: true }
 
     return (
 	<>
-	<div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem', maxWidth: '750px', margin: '0 auto' }}>
-	<LeftSidebar />
-	<div style={{ width: '100%', margin: '0 auto' }}>
-	<BreadcrumbTrail />
-	<PageHeader>{project.title}</PageHeader>
-	<p style={{ color: 'var(--secondary)' }}>{project.date}</p>
-	<div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem', margin: '0.5rem 0 1rem 0' }}>
-	{project.tags.map((tag: string) => (
-	    <InternalLink key={tag} useBubbleStyle={true} href={`/tags/${tag}`}>#{tag}</InternalLink>
-	))}
-	</div>
-	{project?.live && <div className='font-bold'>Live: <ExternalLink href={project.live}>{project.live}</ExternalLink></div>}
-	{project?.source && <div className='font-bold'>Repository: <ExternalLink href={project.source}>{project.source}</ExternalLink></div>}
-	<PostHeader headerNumber={1.5} >Technologies Used</PostHeader>
-	<div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem', margin: '0.5rem 0 1rem 0' }}>
-	{project.technologyUsed.map((technology: string) => (
-	    <InternalLink key={technology} useBubbleStyle={true} href={`/tags/${technology}`}>#{technology}</InternalLink>
-	))}
-	</div>
-	<MarkdownRenderer content={project.content} />        
-	{project?.gallery?.length > 0 && 
-	    <div>
-	<PostHeader headerNumber={1.5}>Gallery</PostHeader>
-	<div style={{ display: 'flex', flexWrap: 'wrap' }}>
-	{project.gallery.map((imagePath: string) => {
-	    return <Image key={imagePath} src={project.gallery_path.split('public')[1] + imagePath} alt={imagePath} className='sm-display-none scale-image' style={{ width: '45%', height: 'auto', borderRadius: '0.5rem', border: '1px solid var(--secondary-light)', margin: '0.5rem' }} />
-	})}	
-	{project.gallery.map((imagePath: string) => {
-	    return <Image key={imagePath} src={project.gallery_path.split('public')[1] + imagePath} alt={imagePath} className='sm-display' style={{ width: '100%', height: 'auto', borderRadius: '0.5rem', border: '1px solid var(--secondary-light)', margin: '0.5rem 0 0.5rem 0' }} hideModal={true} />
-	})}
-	</div>
-	</div>
-	}
-	</div>
-	</div>
-	<RightSidebar post={project} />
-	<BorderLine />
-	<ContentFooter post={project} />
+	    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem', maxWidth: '750px', margin: '0 auto' }}>
+		<LeftSidebar />
+		<div style={{ width: '100%', margin: '0 auto' }}>
+		    <BreadcrumbTrail />
+		    <PageHeader>{project.title}</PageHeader>
+		    <p style={{ color: 'var(--secondary)' }}>{project.date}</p>
+		    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem', margin: '0.5rem 0 1rem 0' }}>
+			{project.tags.map((tag: string) => (
+			    <InternalLink key={tag} useBubbleStyle={true} href={`/tags/${tag}`}>#{tag}</InternalLink>
+			))}
+		    </div>
+		    {project?.live && <div className='font-bold'>Live: <ExternalLink href={project.live}>{project.live}</ExternalLink></div>}
+		    {project?.source && <div className='font-bold'>Repository: <ExternalLink href={project.source}>{project.source}</ExternalLink></div>}
+		    <PostHeader headerNumber={1.5} >Technologies Used</PostHeader>
+		    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem', margin: '0.5rem 0 1rem 0' }}>
+			{project.technologyUsed.map((technology: string) => (
+			    <InternalLink key={technology} useBubbleStyle={true} href={`/tags/${technology}`}>#{technology}</InternalLink>
+			))}
+		    </div>
+		    <MarkdownRenderer content={project.content} />        
+		    {project?.gallery?.length > 0 && 
+			<div>
+			    <PostHeader headerNumber={1.5}>Gallery</PostHeader>
+			    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+				{project.gallery.map((imagePath: string) => {
+				    return <Image key={imagePath} src={project.gallery_path.split('public')[1] + imagePath} alt={imagePath} className='sm-display-none scale-image' style={{ width: '45%', height: 'auto', borderRadius: '0.5rem', border: '1px solid var(--secondary-light)', margin: '0.5rem' }} />
+				})}	
+				{project.gallery.map((imagePath: string) => {
+				    return <Image key={imagePath} src={project.gallery_path.split('public')[1] + imagePath} alt={imagePath} className='sm-display' style={{ width: '100%', height: 'auto', borderRadius: '0.5rem', border: '1px solid var(--secondary-light)', margin: '0.5rem 0 0.5rem 0' }} hideModal={true} />
+				})}
+			    </div>
+			</div>
+		    }
+		</div>
+	    </div>
+	    <RightSidebar post={project} />
+	    <BorderLine />
+	    <ContentFooter post={project} />
 	</>
     )
 }
